9119b89dbc1ec1f9ce9ed17ccf28a328dbfc499a
Danilo Sato <dtsato@gmail.com>
2010-02-08 11:14:35 +0000

    Rewritting javascript for usability enhancements (wasn't working on IE)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,32 @@
-function disableOptionsExcept(selectSelector, optionValue) {
-  $(selectSelector).find('option').each(function() {
-    if ($(this).val() === optionValue) {
-      $(this).removeAttr('disabled').attr('selected', 'selected');
-    } else {
-      $(this).attr('disabled', 'disabled').removeAttr('selected');
-    }
-  });
-}
-
-function enableOptions(selectSelector) {
-  $(selectSelector).find('option').each(function() {
-    $(this).removeAttr('disabled')
+jQuery.fn.bindSelectUpdated = function() {
+  return this.each(function() {
+    $(this).change(function() {$(this).trigger('updated')});
+    $(this).keyup(function() {$(this).trigger('updated')});
   })
 }
+
+jQuery.fn.filterOn = function(field, options_to_hide) {
+  return this.each(function() {
+    var select = this;
+    var options = [];
+    $(select).find('option').each(function() {
+      options.push($(this));
+    });
+    $(select).data('options', options);
+    $(select).data('selected', $(select).val());
+    $(field).bind('updated', function() {
+      var options = $(select).empty().data('options');
+      var hide_options = [];
+      if (options_to_hide.hasOwnProperty($(this).val())) {
+        hide_options = options_to_hide[$(this).val()];
+      }
+      $.each(options, function() {
+        var option = $(this);
+        if($.inArray(option.val(), hide_options) === -1) {
+          $(select).append(option);
+        }
+      });
+      $(select).val($(select).data('selected'));
+    });            
+  });
+};
