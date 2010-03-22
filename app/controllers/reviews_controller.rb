00d174e21c09b1ad3cce2f387d4848c6427bdd78
Hugo Corbucci <hugo.corbucci@gmail.com>
2010-03-22 19:09:27 -0300

    Acertando as rotas e testes de acordo. Views também. Acertando algumas visualizações. Hugo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,6 @@
 class ReviewsController < InheritedResources::Base
   actions :index, :new, :create, :show
   respond_to :html
+  
+  belongs_to :session
 end
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -12,12 +12,12 @@ describe ReviewsController do
   end
   
   it "show action should render show template" do
-    get :show, :id => Review.first
+    get :show, :id => Review.first, :session_id => Session.first
     response.should render_template(:show)
   end
   
   it "new action should render new template" do
-    get :new
+    get :new, :session_id => Session.first
     response.should render_template(:new)
   end
 
@@ -25,13 +25,13 @@ describe ReviewsController do
     # +stubs(:valid?).returns(false)+ doesn't work here because
     # inherited_resources does +obj.errors.empty?+ to determine
     # if validation failed
-    post :create, :review => {}
+    post :create, :review => {}, :session_id => Session.first
     response.should render_template(:new)
   end
   
   it "create action should redirect when model is valid" do
     Review.any_instance.stubs(:valid?).returns(true)
-    post :create
-    response.should redirect_to(review_url(assigns[:review]))
+    post :create, :session_id => Session.first
+    response.should redirect_to(session_review_url(assigns[:session], assigns[:review]))
   end
 end
