3dcade0a54c29d9c6edfb57380749270f9675f4f
Danilo Sato <dtsato@gmail.com>
2010-04-22 00:38:53 -0400

    Adding dependency on chronic and vendoring

--- config/environment.rb
+++ config/environment.rb
@@ -31,6 +31,7 @@ Rails::Initializer.run do |config|
   config.gem 'acts_as_commentable'
   config.gem 'state_machine'
   config.gem 'metric_fu', :lib => false
+  config.gem 'chronic', :lib => false
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
