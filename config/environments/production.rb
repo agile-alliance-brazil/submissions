957c9c6ddbd88e4456ccc51eb0487d7083f59202
Danilo Sato <dtsato@gmail.com>
2011-04-30 03:00:00 -0500

    Migrating e-mail service from SMTP to AWS SES

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,6 +36,7 @@ AgileBrazil::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :ses
 
   # Enable threaded mode
   # config.threadsafe!
---
+++ config/initializers/ses.rb
@@ -0,0 +1,5 @@
+if AppConfig[:ses]
+  ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
+        :access_key_id     => AppConfig[:ses][:access_key_id],
+        :secret_access_key => AppConfig[:ses][:secret_access_key]
+end
