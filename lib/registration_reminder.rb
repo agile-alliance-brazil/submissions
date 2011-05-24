396b45f780e9ae81b29c226bd4856160569afa9c
Danilo Sato <dtsato@gmail.com>
2011-05-23 22:12:50 -0400

    Updating TODO list and adding sleep to avoid throtling error on amazon SES

--- lib/registration_reminder.rb
+++ lib/registration_reminder.rb
@@ -4,13 +4,14 @@ class RegistrationReminder
       Rails.logger.info("[ATTENDEE] #{attendee.to_param}")
       try_with("REMINDER") do
         EmailNotifications.registration_reminder(attendee).deliver
+        sleep(5)
       end
     end
   end
   
   private
   def pending_attendees
-    Attendee.all(:conditions => ['conference_id = ? AND status = ? AND registration_type_id <> ? AND registration_date < ?', current_conference.id, 'pending', RegistrationType.find_by_title('registration_type.group').id, Time.zone.local(2011, 5, 21)])
+    Attendee.all(:conditions => ['conference_id = ? AND status = ? AND registration_type_id <> ? AND registration_date < ?', current_conference.id, 'pending', RegistrationType.find_by_title('registration_type.group').id, Time.zone.local(2011, 5, 21)], :order => 'id')
   end
   
   def try_with(action, &blk)
