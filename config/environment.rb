69538cd536b18906508fcf7c7bca13fb29a2a633
Danilo Sato <dtsato@gmail.com>
2012-01-29 02:05:50 -0500

    Trying to get deploy to dreamhost to work

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,7 @@
+if ENV['RAILS_ENV'] == 'production'
+  ENV['GEM_PATH'] = File.expand_path("~/.gems") + ":/usr/lib/ruby/gems/1.8"
+end
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
