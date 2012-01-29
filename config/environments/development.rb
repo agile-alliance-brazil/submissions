8887fe2159f62b4d137a255896b444bc97db83e4
Danilo Sato <dtsato@gmail.com>
2012-01-29 15:27:40 -0500

    Upgrading rails config files to 3.1, keeping asset pipeline disabled for now

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ AgileBrazil::Application.configure do
 
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
-  # since you don't have to restart the webserver when you make code changes.
+  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Log error messages when you accidentally call methods on nil.
@@ -21,6 +21,12 @@ AgileBrazil::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  # Do not compress assets
+  config.assets.compress = false
+  
+  # Expands the lines which load the assets
+  config.assets.debug = true
 end
 
 # This is just for testing that error pages are generated properly
