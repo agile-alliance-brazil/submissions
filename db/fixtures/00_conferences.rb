ce5ceba25ae819d9ce4a0e44e1e5e40a4e186815
Danilo Sato <dtsato@gmail.com>
2012-02-21 22:40:57 -0500

    Adding fixture for conferences, and updating static pages and images for 2012

---
+++ db/fixtures/00_conferences.rb
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+Conference.seed do |conference|
+  conference.id = 1
+  conference.name = 'Agile Brazil 2010'
+  conference.year = 2010
+end
+
+Conference.seed do |conference|
+  conference.id = 2
+  conference.name = 'Agile Brazil 2011'
+  conference.year = 2011
+end
+
+Conference.seed do |conference|
+  conference.id = 3
+  conference.name = 'Agile Brazil 2012'
+  conference.year = 2012
+end
