f158ef67c29326290dc39cdb2cb53d983b279b2f
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2012-03-11 11:37:27 -0500

    Closes #31. To add a supported language, change the config.

--- config/initializers/00_app_config.rb
+++ config/initializers/00_app_config.rb
@@ -5,6 +5,14 @@ begin
   ::AppConfig = config
   ActionMailer::Base.smtp_settings = config[:smtp_settings]
   ActionMailer::Base.default_url_options[:host] = config[:host]
+
+  module ActionView
+    module Helpers
+      module FormOptionsHelper
+        SUPPORTED_LANGUAGES = [['Português', 'pt'], ['English', 'en']]
+      end
+    end
+  end
 rescue
   raise "config/config.yml file not found. Please check config/config.example for a sample"
 end
