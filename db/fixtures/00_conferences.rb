20484fde5d8a483b814f19a5782d716f53c5211f
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2012-03-19 20:38:07 -0700

    Adjusting dates for 2012 conference.

--- db/fixtures/00_conferences.rb
+++ db/fixtures/00_conferences.rb
@@ -29,11 +29,11 @@ Conference.seed do |conference|
   conference.id                   = 3
   conference.name                 = 'Agile Brazil 2012'
   conference.year                 = 2012
-  conference.location_and_date    = 'São Paulo SP, Jun - Jul, 2012'
+  conference.location_and_date    = 'São Paulo SP, 3-7 Set, 2012'
   conference.call_for_papers      = Time.zone.local(2012, 2, 5)
-  conference.submissions_open     = Time.zone.local(2012, 3, 7)
-  conference.submissions_deadline = Time.zone.local(2012, 4, 6, 23, 59, 59)
-  conference.review_deadline      = Time.zone.local(2012, 4, 28, 23, 59, 59)
-  conference.author_notification  = Time.zone.local(2012, 5, 10, 23, 59, 59)
-  conference.author_confirmation  = Time.zone.local(2012, 5, 25, 23, 59, 59)
+  conference.submissions_open     = Time.zone.local(2012, 3, 22)
+  conference.submissions_deadline = Time.zone.local(2012, 5, 27, 23, 59, 59)
+  conference.review_deadline      = Time.zone.local(2012, 6, 17, 23, 59, 59)
+  conference.author_notification  = Time.zone.local(2012, 7, 8, 23, 59, 59)
+  conference.author_confirmation  = Time.zone.local(2012, 7, 22, 23, 59, 59)
 end
