27438e5c8780011505904412c26ebca6c88d3f6b
Danilo Sato <dtsato@gmail.com>
2012-03-21 01:03:48 -0400

    Fixing migration to not perform validation

--- db/migrate/20120221051328_remove_registrar_role_from_users.rb
+++ db/migrate/20120221051328_remove_registrar_role_from_users.rb
@@ -2,7 +2,7 @@ class RemoveRegistrarRoleFromUsers < ActiveRecord::Migration
   def up
     User.all.each do |user|
       user.remove_role :registrar
-      user.save!
+      user.save(:validate => false)
     end
   end
 
