befc14fca5f00895d1e8b5c2045340b742c0c1d2
Danilo Sato <dtsato@gmail.com>
2012-04-23 17:18:51 -0400

    Moving secret token to config file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AgileBrazil::Application.config.secret_token = 'cd8960c2cf59235252ed56d75e354598729e4eecfdd7fce80e22b88e5b79a630f2a65cb67465411079931adad1fd416ed82d69fa1d941f9f4ad4f905e47a1ef9'
+AgileBrazil::Application.config.secret_token = AppConfig[:secret_token]
