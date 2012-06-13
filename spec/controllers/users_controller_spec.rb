87718ca614f3a50751aa6d4cf543ec03d9efffb2
Danilo Sato <dtsato@gmail.com>
2012-06-12 23:56:48 -0400

    Loading fixtures explicitly to fix the build

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,28 +2,31 @@
 require 'spec_helper'
 
 describe UsersController, :render_views => true do
+  fixtures :users
   render_views
-  
+
   it "show should work" do
     get :show, :id => User.first
   end
 end
 
 describe UsersController do
+  fixtures :users
+
   describe "#index" do
     describe "with javascript format" do
       before do
         get :index, :format => :js, :term => 'dt'
       end
-      
+
       it { should respond_with_content_type(:js) }
     end
-    
+
     describe "with html format" do
       before do
         get :index
       end
-      
+
       it { should redirect_to(new_user_registration_path) }
     end
   end
@@ -32,7 +35,7 @@ describe UsersController do
     before do
       get :show, :id => User.first
     end
-    
+
     it { should respond_with(:success) }
     it { should render_template(:show) }
   end
