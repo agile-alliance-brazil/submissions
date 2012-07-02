8f74ffdcea5f1aeec82cc3cb5c5e7b8ef6519a2b
Danilo Sato <dtsato@gmail.com>
2012-07-02 20:51:45 -0300

    Adding a few more checks prior to publishing reviews

--- lib/review_publisher.rb
+++ lib/review_publisher.rb
@@ -3,6 +3,7 @@ class ReviewPublisher
   def publish
     ensure_all_sessions_reviewed
     ensure_all_decisions_made
+    ensure_all_decisions_consistent_with_session_state
     rejected_sessions.each do |session|
       Rails.logger.info("[SESSION] #{session.to_param}")
       try_with("REJECT") do
@@ -38,6 +39,30 @@ class ReviewPublisher
                 ON review_decision_count.session_id = sessions.id",
       :conditions => ['state IN (?) AND review_decision_count.cnt <> 1 AND conference_id = ?', ['pending_confirmation', 'rejected'], current_conference.id])
     raise "There are #{missing_decision} sessions without decision" if missing_decision > 0
+  end
+
+  def ensure_all_decisions_consistent_with_session_state
+    accepted_in_wrong_state = Session.count(
+      :joins => "left outer join (
+                  SELECT session_id, count(*) AS cnt
+                  FROM review_decisions
+                  WHERE review_decisions.outcome_id = 1
+                  GROUP BY session_id
+                ) AS accepted_count
+                ON accepted_count.session_id = sessions.id",
+      :conditions => ['state NOT IN (?) AND accepted_count.cnt > 0 AND conference_id = ?', ['pending_confirmation', 'accepted'], current_conference.id])
+    raise "There are #{accepted_in_wrong_state} accepted sessions with the wrong state" if accepted_in_wrong_state > 0
+
+    rejected_in_wrong_state = Session.count(
+      :joins => "left outer join (
+                  SELECT session_id, count(*) AS cnt
+                  FROM review_decisions
+                  WHERE review_decisions.outcome_id = 2
+                  GROUP BY session_id
+                ) AS rejected_count
+                ON rejected_count.session_id = sessions.id",
+      :conditions => ['state NOT IN (?) AND rejected_count.cnt > 0 AND conference_id = ?', ['rejected'], current_conference.id])
+    raise "There are #{rejected_in_wrong_state} rejected sessions with the wrong state" if rejected_in_wrong_state > 0
   end
 
   def rejected_sessions
