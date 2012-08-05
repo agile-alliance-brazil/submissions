1cc9ead6cf5c62bb0fa020bc5d510217ece8df30
Danilo Sato <dtsato@gmail.com>
2012-08-04 21:57:49 -0500

    Removing localized_country_select plugin and replacing with gem

--- vendor/plugins/localized_country_select/init.rb
+++
@@ -1,5 +0,0 @@
-# Require the plugin code
-require 'localized_country_select'
-
-# Load locales for countries from +locale+ directory into Rails
-I18n.load_path += Dir[ File.join(File.dirname(__FILE__), 'locale', '*.{rb,yml}') ]
