1033d38c38708ae6fd93b97343908b160bb1879c
Danilo Sato <dtsato@gmail.com>
2012-08-10 12:47:07 -0500

    Reverting last attempts, and trying to start xvfb for Travis CI

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,10 +35,4 @@ AgileBrazil::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-
-  # Do not compress assets
-  config.assets.compress = false
-
-  # Expands the lines which load the assets
-  config.assets.debug = true
 end
