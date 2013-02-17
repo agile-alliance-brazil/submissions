704b0add6aea0fed607f45994b9a48f036a97b45
Hugo Corbucci <hugo.corbucci@gmail.com>
2013-02-17 15:33:52 -0600

    Fixing dates to the current plan.

--- db/fixtures/00_conferences.rb
+++ db/fixtures/00_conferences.rb
@@ -50,8 +50,8 @@ Conference.seed do |conference|
   conference.presubmissions_deadline   = nil# No pre review
   conference.prereview_deadline        = nil# No pre review
   conference.submissions_deadline      = Time.zone.local(2013, 4, 7, 23, 59, 59)
-  conference.review_deadline           = Time.zone.local(2013, 5, 15, 23, 59, 59)
-  conference.author_notification       = Time.zone.local(2013, 6, 6, 23, 59, 59)
-  conference.author_confirmation       = Time.zone.local(2013, 6, 14, 23, 59, 59)
+  conference.review_deadline           = Time.zone.local(2013, 4, 26, 23, 59, 59)
+  conference.author_notification       = Time.zone.local(2013, 5, 15, 23, 59, 59)
+  conference.author_confirmation       = Time.zone.local(2013, 6, 2, 23, 59, 59)
 end
 
