65543e4a037ade303d7b1791ffe6461e79821b85
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2013-04-01 20:01:56 -0300

    redirecionando para a página original quando vota ou cancela o voto

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,7 +7,14 @@ class VotesController < ApplicationController
     @vote = Vote.new params[:vote]
     can_vote = @vote.try(:session).can_be_voted_by?(current_user)
     if can_vote && @vote.save
-      redirect_to sessions_path(@vote.year)
+      respond_to do |format|
+        format.html { redirect_to request.referer }
+        format.js { head :ok }
+      end
+    else
+      respond_to do |format|
+        format.js { head :internal_server_error }
+      end
     end
   end
 
@@ -15,7 +22,14 @@ class VotesController < ApplicationController
     @vote = Vote.find params[:id]
     year = @vote.year
     if @vote.destroy
-      redirect_to sessions_path(year)
+      respond_to do |format|
+        format.html { redirect_to request.referer }
+        format.js { head :ok }
+      end
+    else
+      respond_to do |format|
+        format.js { head :internal_server_error }
+      end
     end
   end
 end
