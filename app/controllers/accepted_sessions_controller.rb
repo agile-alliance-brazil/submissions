a09376422c63840eec113b7ed73cb5b0621fd0bc
Ceci Fernandes <ceci.vidageek@gmail.com>
2014-01-14 15:52:13 -0200

    Making accepted_sessions export activities as json. I'll use it elsewhere

--- app/controllers/accepted_sessions_controller.rb
+++ app/controllers/accepted_sessions_controller.rb
@@ -3,8 +3,13 @@ class AcceptedSessionsController < InheritedResources::Base
   skip_before_filter :authenticate_user!
   append_view_path ActivitiesResolver.new
 
+  respond_to :json, :html
+
   def index
     @activities = Activity.for_conference(@conference)
-    render :layout => false
+    respond_with(@activities) do |format|
+      format.html {render :layout => false}
+      format.json {render :json => @activities.to_json}
+    end
   end
 end
