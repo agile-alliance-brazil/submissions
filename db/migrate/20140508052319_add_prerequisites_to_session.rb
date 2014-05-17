66a8567536cc7147dc6ee15d1713201482ecaf6f
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-05-17 15:40:09 -0500

    Fixing duplicated seeds and references

--- db/migrate/20140508052319_add_prerequisites_to_session.rb
+++ db/migrate/20140508052319_add_prerequisites_to_session.rb
@@ -1,6 +1,6 @@
 class AddPrerequisitesToSession < ActiveRecord::Migration
   def change
     add_column :sessions, :prerequisites, :string
-    execute "update sessions set prerequisites='Este campo foi criado na AgileBrazil 2014'"
+    execute 'update sessions set prerequisites="Este campo foi criado na AgileBrazil 2014"'
   end
 end
---
+++ db/migrate/20140517173937_unify_doubled_seeds.rb
@@ -0,0 +1,27 @@
+class UnifyDoubledSeeds < ActiveRecord::Migration
+  def up
+    seeded_tags = ActsAsTaggableOn::Tag.where('expiration_year IS NULL').all
+    seeded_tags_map = seeded_tags.group_by{|tag| tag.name}
+    tags_to_keep = seeded_tags_map.values.map{|tags| tags.first}
+    id_map = tags_to_keep.inject({}) do |acc, keep|
+      acc[keep.id] = seeded_tags.select{|tag| tag.name == keep.name && tag.id != keep.id}.map(&:id)
+      acc
+    end
+    duplicated_ids = []
+    id_map.each do |key, value|
+      ActsAsTaggableOn::Tagging.where(:tag_id => value).update_all(:tag_id => key)
+      taggings = ActsAsTaggableOn::Tagging.where(:tag_id => key).all
+      grouped = taggings.group_by do |tagging|
+        [tagging.taggable_id, tagging.taggable_type, tagging.context]
+      end
+      duplicated_ids << grouped.values.map{|dup| dup[1..-1].map{|d| d.id}}
+    end
+    duplicated_ids
+    ActsAsTaggableOn::Tagging.delete_all(:id => duplicated_ids.flatten)
+    ActsAsTaggableOn::Tag.delete_all(:id => id_map.values.flatten)
+  end
+
+  def down
+    # Can't down data drop
+  end
+end
