8423ecc4270018f0c27967033580110fe1843595
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2014-09-08 09:51:34 -0500

    Adding server configuration for SES
    Since it will by default try west1 as the region and it won't work if
    you're not in that region.

--- config/initializers/ses.rb
+++ config/initializers/ses.rb
@@ -2,5 +2,6 @@
 if AppConfig[:ses]
   ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
         :access_key_id     => AppConfig[:ses][:access_key_id],
-        :secret_access_key => AppConfig[:ses][:secret_access_key]
+        :secret_access_key => AppConfig[:ses][:secret_access_key],
+        :server            => AppConfig[:ses][:server]
 end
