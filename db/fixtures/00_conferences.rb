521244664c19b0e8ded82462574896e402fc376b
Ceci Fernandes <ceci.vidageek@gmail.com>
2015-07-20 00:00:01 -0300

    Postponing reviewers' deadline for two more days

--- db/fixtures/00_conferences.rb
+++ db/fixtures/00_conferences.rb
@@ -82,7 +82,7 @@ Conference.seed do |conference|
   conference.presubmissions_deadline   = nil# No pre review
   conference.prereview_deadline        = nil# No pre review
   conference.submissions_deadline      = Time.zone.local(2015, 6, 28, 23, 59, 59)
-  conference.review_deadline           = Time.zone.local(2015, 7, 19, 23, 59, 59)
+  conference.review_deadline           = Time.zone.local(2015, 7, 21, 23, 59, 59)
   conference.voting_deadline           = nil# No voting
   conference.author_notification       = Time.zone.local(2015, 7, 29, 23, 59, 59)
   conference.author_confirmation       = Time.zone.local(2015, 8, 10, 2, 59, 59)
