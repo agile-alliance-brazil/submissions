007b2a69caceb0640209e7f3d70c2c2156052bcb
Hugo Corbucci <hugo.corbucci@gmail.com>
2018-03-17 16:00:23 -0300

    Should solve #259
    Now the autocomplete suggestions consider the locale the user
    is on as well as the conference if any is specified.
    It also return results on translated values instead of the key
    in the translation file which makes a lot more sense.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,9 +4,24 @@ class TagsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    collection = ActsAsTaggableOn::Tag.named_like(params[:term]).all
+    year = params[:year]
+    collection = []
+    term = params[:term]
+    tags = year ? @conference.tags.all : resource_class.all
+    tag_names = tags.map do |tag|
+      if I18n.exists?(tag.name, I18n.locale)
+        I18n.t(tag.name)
+      else
+        tag.name
+      end
+    end
+    regex = /.*#{term}.*/
+    collection = tag_names.select do |tag_name|
+      regex.match(tag_name)
+    end
+
     respond_to do |format|
-      format.json { render json: collection.map(&:name) }
+      format.json { render json: collection }
     end
   end
 
