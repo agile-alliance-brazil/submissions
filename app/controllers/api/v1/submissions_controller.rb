4698d7fec2089a708cea299ba7c0a8a6c7186a72
Hugo Corbucci <hugo.corbucci@gmail.com>
2018-04-22 22:15:05 -0400

    fixing rubocop

--- app/controllers/api/v1/submissions_controller.rb
+++ app/controllers/api/v1/submissions_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class SubmissionsController < ::ApplicationController
@@ -10,23 +12,21 @@ module Api
 
       def index
         conferences = Conference.all
-        length = conferences.map{|c| (c.submissions_deadline.to_date - c.submissions_open.to_date).to_i }.max
+        length = conferences.map { |c| (c.submissions_deadline.to_date - c.submissions_open.to_date).to_i }.max
 
         conf_distribution = conferences.map do |c|
           conference_length = (c.submissions_deadline.to_date - c.submissions_open.to_date).to_i
           buckets = Array.new(length + 1, 0)
-          counts = Session.for_conference(c).active.inject(buckets) do |acc, s|
+          counts = Session.for_conference(c).active.each_with_object(buckets) do |s, acc|
             creation_percent = (s.created_at.to_date - c.submissions_open.to_date).to_i.to_f / conference_length
             creation_bucket = [0, [(creation_percent * length).to_i, length].min].max
             acc[creation_bucket] = 0 unless acc[creation_bucket]
-            acc[creation_bucket]+= 1
-            acc
+            acc[creation_bucket] += 1
           end
 
-          accumulated = counts.each_with_index.inject(Array.new(length + 1, 0)) do |acc, (count, index)|
+          accumulated = counts.each_with_index.each_with_object(Array.new(length + 1, 0)) do |(count, index), acc|
             acc[index] = count
-            acc[index] += acc[index-1] if index > 0
-            acc
+            acc[index] += acc[index - 1] if index.positive?
           end
 
           {
