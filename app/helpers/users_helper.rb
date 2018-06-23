9491ac1a9c41b2d5b3e4fcfa84025aa7e8f2188d
Hugo Corbucci <hugo.corbucci@gmail.com>
2018-06-23 12:45:14 -0400

    That's what you get for doing two things at the same time

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module UsersHelper
+  def previous_sessions(lastest_conference, user, current_user)
+    old_sessions = user.sessions.includes(:final_reviews).reject do |s|
+      s.conference == lastest_conference
+    end
+    old_sessions.map do |s|
+      text = s.title
+      if current_user == user && current_user.organizer_for_conference(latest_conference)
+        reviews = s.final_reviews.map { |r| r.recommendation.to_utf_chars }.join(', ')
+        text += " (#{I18n.t("session.state.#{s.state}")} - #{reviews})".html_safe
+      end
+      link_to text, session_path(s.conference, s)
+    end
+  end
+end
