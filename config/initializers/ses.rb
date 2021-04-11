1a2541c8b1f40a2b6d28924dce128cac5c8ae615
Hugo Corbucci <hugo.corbucci@gmail.com>
2021-04-11 17:32:52 -0300

    upgrading to signature v4 for aws

--- config/initializers/ses.rb
+++ config/initializers/ses.rb
@@ -3,5 +3,6 @@
 if APP_CONFIG[:ses]
   ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
                                          access_key_id: APP_CONFIG[:ses][:access_key_id],
-                                         secret_access_key: APP_CONFIG[:ses][:secret_access_key]
+                                         secret_access_key: APP_CONFIG[:ses][:secret_access_key],
+                                         signature_version: 4
 end
