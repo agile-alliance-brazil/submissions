5dd98fae00bc2aee876f37fa970e409ed2f12045
Pablo Bender <pablo@bender.eti.br>
2022-04-03 09:44:56 -0400

    Add diversity related fields to users table

---
+++ db/migrate/20220329224338_add_diversity_fields_to_users.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class AddDiversityFieldsToUsers < ActiveRecord::Migration
+  def change
+    change_table :users do |t|
+      t.string "gender"
+      t.string "race"
+      t.string "disabilities"
+      t.datetime "date_of_birth"
+      t.string "parenting_type"
+      t.string "geographical_area"
+    end
+  end
+end
