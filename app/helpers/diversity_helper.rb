e098a3150c912ac0764261ec1d5adcebe233663d
Pablo Bender <pablo@bender.eti.br>
2022-04-05 13:00:03 -0400

    rename user diversity fields through application and some refactors

--- app/helpers/diversity_helper.rb
+++ app/helpers/diversity_helper.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 module DiversityHelper
-  COMMON_OPTIONS = %i[rather_not_answer i_dont_know].freeze
-  GENDER_VALUES = %i[cis_man trans_man cis_woman trans_woman non_binary].freeze
-  RACE_VALUES = %i[yellow white indian brown black].freeze
-  DISABILITIES_VALUES = %i[no_disabilities visual hearing physical_or_motor mental_or_intellectual].freeze
-  PARENTING_TYPE_VALUES = %i[yes no].freeze
+  GENDER_VALUES = %i[rather_not_answer cis_man trans_man cis_woman trans_woman non_binary i_dont_know].freeze
+  RACE_VALUES = %i[rather_not_answer yellow white indian brown black i_dont_know].freeze
+  DISABILITY_VALUES = %i[rather_not_answer no_disability visual hearing physical_or_motor mental_or_intellectual i_dont_know].freeze
+  IS_PARENT_VALUES = %i[rather_not_answer yes no].freeze
 
   def gender_options
-    options_for(GENDER_VALUES, :gender, include_i_dont_know: true)
+    options_for(GENDER_VALUES, :gender)
   end
 
   def translated_gender(value)
@@ -16,19 +15,19 @@ module DiversityHelper
   end
 
   def race_options
-    options_for(RACE_VALUES, :race, include_i_dont_know: true)
+    options_for(RACE_VALUES, :race)
   end
 
   def translated_race(value)
     translate_option(value, RACE_VALUES, :race)
   end
 
-  def disabilities_options
-    options_for(DISABILITIES_VALUES, :disabilities, include_i_dont_know: true)
+  def disability_options
+    options_for(DISABILITY_VALUES, :disability)
   end
 
-  def translated_disabilities(value)
-    translate_option(value, DISABILITIES_VALUES, :disabilities)
+  def translated_disability(value)
+    translate_option(value, DISABILITY_VALUES, :disability)
   end
 
   def translated_age_range(a_date)
@@ -37,43 +36,37 @@ module DiversityHelper
     date = a_date.beginning_of_day
     now = DateTime.current.beginning_of_day
 
-    return 'Até 18 anos' if date.advance(years: 19) > now
-    return '19 a 24 anos' if date.advance(years: 25) > now
-    return '25 a 29 anos' if date.advance(years: 30) > now
-    return '30 a 34 anos' if date.advance(years: 35) > now
-    return '35 a 39 anos' if date.advance(years: 40) > now
-    return '40 a 44 anos' if date.advance(years: 45) > now
-    return '45 a 49 anos' if date.advance(years: 50) > now
-    return '50 a 54 anos' if date.advance(years: 55) > now
-    return '55 a 59 anos' if date.advance(years: 60) > now
+    return t('age_range.until_18') if date.advance(years: 19) > now
+    return t('age_range.between_19_and_24') if date.advance(years: 25) > now
+    return t('age_range.between_25_and_29') if date.advance(years: 30) > now
+    return t('age_range.between_30_and_34') if date.advance(years: 35) > now
+    return t('age_range.between_35_and_39') if date.advance(years: 40) > now
+    return t('age_range.between_40_and_44') if date.advance(years: 45) > now
+    return t('age_range.between_45_and_49') if date.advance(years: 50) > now
+    return t('age_range.between_50_and_54') if date.advance(years: 55) > now
+    return t('age_range.between_55_and_59') if date.advance(years: 60) > now
 
     '60 anos ou mais'
   end
 
-  def parenting_type_options
-    options_for(PARENTING_TYPE_VALUES, :parenting_type, include_i_dont_know: false)
+  def is_parent_options
+    options_for(IS_PARENT_VALUES, :is_parent)
   end
 
-  def translated_parenting_type(value)
-    translate_option(value, PARENTING_TYPE_VALUES, :parenting_type)
+  def translated_is_parent(value)
+    translate_option(value, IS_PARENT_VALUES, :is_parent)
   end
 
   private
 
-  def options_for(values, scope, options)
-    result = [[t(:rather_not_answer, scope: :generic), :rather_not_answer]] + values.map { |item| [t(item, scope: scope), item] }
-
-    return result + [[t(:i_dont_know, scope: :generic), :i_dont_know]] if options[:include_i_dont_know]
-
-    result
+  def options_for(values, scope)
+    values.map { |item| [t(item, scope: scope), item] }
   end
 
   def translate_option(option, valid_values, scope)
     return '' if option.blank?
 
     return I18n.translate(option, scope: scope) if valid_values.include?(option.to_sym)
-
-    return I18n.translate(option, scope: :generic) if COMMON_OPTIONS.include?(option.to_sym)
 
     ''
   end
