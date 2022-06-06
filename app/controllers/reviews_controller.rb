5e9528866e9e77aa3d5c49edd7707795759a785b
Pablo Bender <pablo@bender.eti.br>
2022-06-06 12:18:10 -0400

    test auto-review for rejected sessions on staging

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -136,7 +136,7 @@ class ReviewsController < ApplicationController
     resource_class.create!(
       session: review.session,
       recommendation: Recommendation.find_by(name: 'weak_reject'),
-      reviewer_id: 872,
+      reviewer_id: 99,
       author_agile_xp_rating: Rating.find_low_instance,
       author_proposal_xp_rating: Rating.find_low_instance,
       proposal_quality_rating: Rating.find_low_instance,
