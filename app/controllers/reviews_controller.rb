3e2842ccd582577031ebbd4cc13704529be1c887
Pablo Bender <pablo@bender.eti.br>
2022-07-19 12:02:18 -0400

    fix user used as reviewer_id on 3rd automatic reject review when session receives first 2 manual reject reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -136,7 +136,7 @@ class ReviewsController < ApplicationController
     resource_class.create!(
       session: review.session,
       recommendation: Recommendation.find_by(name: 'weak_reject'),
-      reviewer_id: 1199,
+      reviewer_id: 3202,
       author_agile_xp_rating: Rating.find_low_instance,
       author_proposal_xp_rating: Rating.find_low_instance,
       proposal_quality_rating: Rating.find_low_instance,
